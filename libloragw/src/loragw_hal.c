e6036f5c2cd7acad95fd27333b69a7679afd3139
george <george@cool-pi.com>
2025-08-21 17:55:44 +0800

    support no temperature amd adc modules

--- libloragw/src/loragw_hal.c
+++ libloragw/src/loragw_hal.c
@@ -201,14 +201,14 @@ static lgw_context_t lgw_context = {
 
 /* File handle to write debug logs */
 FILE * log_file = NULL;
-
+#if 0
 /* I2C temperature sensor handles */
 static int     ts_fd = -1;
 static uint8_t ts_addr = 0xFF;
 
 /* I2C AD5338 handles */
 static int     ad_fd = -1;
-
+#endif
 /* -------------------------------------------------------------------------- */
 /* --- PRIVATE FUNCTIONS DECLARATION ---------------------------------------- */
 
@@ -1093,6 +1093,7 @@ int lgw_start(void) {
     dbg_init_random();
 
     if (CONTEXT_COM_TYPE == LGW_COM_SPI) {
+#if 0
         /* Find the temperature sensor on the known supported ports */
         for (i = 0; i < (int)(sizeof I2C_PORT_TEMP_SENSOR); i++) {
             ts_addr = I2C_PORT_TEMP_SENSOR[i];
@@ -1116,7 +1117,6 @@ int lgw_start(void) {
             printf("ERROR: no temperature sensor found.\n");
             return LGW_HAL_ERROR;
         }
-
         /* Configure ADC AD338R for full duplex (CN490 reference design) */
         if (CONTEXT_BOARD.full_duplex == true) {
             err = i2c_linuxdev_open(I2C_DEVICE, I2C_PORT_DAC_AD5338R, &ad_fd);
@@ -1142,6 +1142,7 @@ int lgw_start(void) {
             }
             printf("INFO: AD5338R: Set DAC output to 0x%02X 0x%02X\n", (uint8_t)VOLTAGE2HEX_H(0), (uint8_t)VOLTAGE2HEX_L(0));
         }
+#endif
     }
 
     /* Connect to the external sx1261 for LBT or Spectral Scan */
@@ -1220,8 +1221,9 @@ int lgw_stop(void) {
         printf("ERROR: failed to disconnect concentrator\n");
         err = LGW_HAL_ERROR;
     }
-
+ 
     if (CONTEXT_COM_TYPE == LGW_COM_SPI) {
+#if 0
         DEBUG_MSG("INFO: Closing I2C for temperature sensor\n");
         x = i2c_linuxdev_close(ts_fd);
         if (x != 0) {
@@ -1237,6 +1239,7 @@ int lgw_stop(void) {
                 err = LGW_HAL_ERROR;
             }
         }
+#endif
     }
 
     CONTEXT_STARTED = false;
@@ -1285,14 +1288,14 @@ int lgw_receive(uint8_t max_pkt, struct lgw_pkt_rx_s *pkt_data) {
         nb_pkt_left = nb_pkt_fetched - max_pkt;
         printf("WARNING: not enough space allocated, fetched %d packet(s), %d will be left in RX buffer\n", nb_pkt_fetched, nb_pkt_left);
     }
-
+#if 0
     /* Apply RSSI temperature compensation */
     res = lgw_get_temperature(&current_temperature);
     if (res != LGW_I2C_SUCCESS) {
         printf("ERROR: failed to get current temperature\n");
         return LGW_HAL_ERROR;
     }
-
+#endif
     /* Iterate on the RX buffer to get parsed packets */
     for (nb_pkt_found = 0; nb_pkt_found < ((nb_pkt_fetched <= max_pkt) ? nb_pkt_fetched : max_pkt); nb_pkt_found++) {
         /* Get packet and move to next one */
@@ -1410,7 +1413,7 @@ int lgw_send(struct lgw_pkt_tx_s * pkt_data) {
         printf("ERROR: INVALID TX MODULATION\n");
         return LGW_HAL_ERROR;
     }
-
+#if 0
     /* Set PA gain with AD5338R when using full duplex CN490 ref design */
     if (CONTEXT_BOARD.full_duplex == true) {
         uint8_t volt_val[AD5338R_CMD_SIZE] = {0x39, VOLTAGE2HEX_H(2.51), VOLTAGE2HEX_L(2.51)}; /* set to 2.51V */
@@ -1421,7 +1424,7 @@ int lgw_send(struct lgw_pkt_tx_s * pkt_data) {
         }
         printf("INFO: AD5338R: Set DAC output to 0x%02X 0x%02X\n", (uint8_t)VOLTAGE2HEX_H(2.51), (uint8_t)VOLTAGE2HEX_L(2.51));
     }
-
+#endif
     /* Start Listen-Before-Talk */
     if (CONTEXT_SX1261.lbt_conf.enable == true) {
         err = lgw_lbt_start(&CONTEXT_SX1261, pkt_data);
@@ -1589,7 +1592,7 @@ int lgw_get_eui(uint64_t* eui) {
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 int lgw_get_temperature(float* temperature) {
-    int err = LGW_HAL_ERROR;
+    int err = LGW_HAL_SUCCESS;
 
     DEBUG_PRINTF(" --- %s\n", "IN");
 
@@ -1597,7 +1600,7 @@ int lgw_get_temperature(float* temperature) {
 
     switch (CONTEXT_COM_TYPE) {
         case LGW_COM_SPI:
-            err = stts751_get_temperature(ts_fd, ts_addr, temperature);
+            //err = stts751_get_temperature(ts_fd, ts_addr, temperature);
             break;
         case LGW_COM_USB:
             err = lgw_com_get_temperature(temperature);
